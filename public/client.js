f8c3afa85ffe1cc0b3496f602289742fcab9a1a6
eko24ive <eko24ive@gmail.com>
2017-04-09 16:40:06 +0300

    removed jquery from dep

--- public/client.js
+++ public/client.js
@@ -1,10 +1,17 @@
 $('#uploadForm').submit(function(e) {
     e.preventDefault();
 
-    $(this).ajaxSubmit({
-        success: function(response) {
-            console.log('image uploaded and form submitted');
-        }
-    });
+
+    $.ajax({
+        url: "/upload",
+        method: "POST",
+        cache: false,
+        contentType: false,
+        processData: false,
+        data: new FormData(jQuery('#uploadForm')[0])
+    }).then(function(res) {
+        console.log(res);
+    })
+
     return false;
 });
